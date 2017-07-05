affc2e2dd0d3404b23b34c9cc4ee709ad5091e84
declan enright <decenright@gmail.com>
2017-07-05 12:01:24 +0100

    Loads Tharstern odbc data to Datagrid on form load

--- PalletCard/MainWindow.xaml.cs
+++ PalletCard/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Odbc;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,5 +27,36 @@ namespace PalletCard
         {
             InitializeComponent();
         }
+
+        private void Dockpanel_Loaded(object sender, RoutedEventArgs e)
+        {
+            string ConnectionString = Convert.ToString("Dsn=TharData;uid=tharuser");
+            string CommandText = "SELECT * FROM app_PalletOperations where resourceID = 5";
+            OdbcConnection myConnection = new OdbcConnection(ConnectionString);
+            OdbcCommand myCommand = new OdbcCommand(CommandText, myConnection);
+            OdbcDataAdapter myAdapter = new OdbcDataAdapter();
+            myAdapter.SelectCommand = myCommand;
+            DataSet tharData = new DataSet();
+            try
+            {
+                myConnection.Open();
+                myAdapter.Fill(tharData);
+            }
+            catch (Exception ex)
+            {
+                throw (ex);
+            }
+            finally
+            {
+                myConnection.Close();
+            }
+            using (DataTable operations = new DataTable())
+            {
+                myAdapter.Fill(operations);
+                dataGrid.ItemsSource = operations.DefaultView;
+                myAdapter.Update(operations);
+            }
+        }
     }
+
 }
